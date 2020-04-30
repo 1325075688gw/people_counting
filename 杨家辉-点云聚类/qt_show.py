a6d5f3580129af93cee37ead39aa130a86153643
WindStormV <896853536@qq.com>
2020-04-30 16:13:41 +0800

    修改qt_show.py 加入了2张yoz显示

--- 杨家辉-点云聚类/qt_show.py
+++ 杨家辉-点云聚类/qt_show.py
@@ -14,17 +14,23 @@ class MainWindow():
         self.mw.setCentralWidget(self.view)
         self.mw.show()
         self.mw.setWindowTitle('Show PointCloud')
-        self.plot = self.view.addPlot()
-        self.scatter = pg.ScatterPlotItem(size=5, pen=pg.mkPen(None), brush=pg.mkBrush(0, 0, 0, 255))
-        self.plot.setRange(xRange=[-4, 4], yRange=[0, 7], padding=0)
+        self.plot1 = self.view.addPlot() #xoy
+        self.plot2 = self.view.addPlot() #yoz
+        self.scatter1 = pg.ScatterPlotItem(size=5, pen=pg.mkPen(None), brush=pg.mkBrush(0, 0, 0, 255))
+        self.scatter2 = pg.ScatterPlotItem(size=5, pen=pg.mkPen(None), brush=pg.mkBrush(0, 0, 0, 255))
+        self.plot1.setRange(xRange=[-4, 4], yRange=[0, 7], padding=0)
+        self.plot2.setRange(xRange=[0, 7], yRange=[0, 2.5], padding=0)
 
     def update_data(self):
         point_cloud = []
         if PointCloud.get_frame_pointcloud(point_cloud, True):
-            spots = [{'pos': np.array(point)} for point in point_cloud]
-            self.scatter.setData(spots)
+            spots1 = [{'pos': np.array(point)} for point in point_cloud]
+            spots2 = [{'pos': np.array([point[1], point[2]])} for point in point_cloud]
+            self.scatter2.setData(spots2)
+            self.scatter1.setData(spots1)
             #print(spots)
-            self.plot.addItem(self.scatter)
+            self.plot1.addItem(self.scatter1)
+            self.plot2.addItem(self.scatter2)
 
     def run(self):
         timer = pg.QtCore.QTimer()
