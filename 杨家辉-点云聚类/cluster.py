e582cca1667e68a2a97c4c0a6fe57fd998e0d2ce
WindStormV <896853536@qq.com>
2020-05-02 15:26:53 +0800

    修改了聚类、滤波、加入了最新身高计算

--- 杨家辉-点云聚类/cluster.py
+++ 杨家辉-点云聚类/cluster.py
@@ -1,220 +1,270 @@
-import numpy as np
+import numpy as np  # 数据结构
 import sklearn.cluster as skc
 import math
+from height import Height
 
 
 class Cluster():
-    def __init__(self, eps, minpts, type, min_cluster_count):
-        self.eps = eps
-        self.minpts = minpts
-        self.type = type
-        self.min_cluster_count = min_cluster_count
-        self.cluster_division_limit = 80  # 达到80个点，对点云分割（分割失败保持原样）
-        self.frame_cluster_dict = {}
+	def __init__(self,eps,minpts,type,min_cluster_count):
+		self.eps = eps
+		self.minpts = minpts
+		self.type = type
+		self.min_cluster_count = min_cluster_count
+		self.cluster_division_limit = 80  #达到80个点，对点云分割（分割失败保持原样）
+		self.frame_cluster_dict = {}
+		'''
+		frame_cluster_dict
+		{
+			'frame_num':
+			'cluster':[
+						{'cluster_id'
+						...
+						},
+						...
+					  ]
+		}
+		'''
 
-    def dbscan_official(self, data):
-        """
-        使用DBSCAN聚类算法对点云数据进行聚类
-        :param data: 点云数据
-        :return: 聚类标签
-        """
-        if data == []:
-            return []
-        X = np.array(data)
-        if self.type == '2D':
-            X = X[:, :2]
-        elif self.type == '3D':
-            X = X[:, :3]
-        db = skc.DBSCAN(eps=self.eps, min_samples=self.minpts).fit(X)
-        return db.labels_
+	def dbscan_official(self, data):
+		if data == []:
+			return []
+		X = np.array(data)
+		if self.type == '2D':
+			X = X[:,:2]
+		elif self.type == '3D':
+			X = X[:,:3]
+		db = skc.DBSCAN(eps=self.eps, min_samples=self.minpts).fit(X)
+		return db.labels_
+	
+	def frame_data_to_cluster_points(self,frame_data):
+		points = []
+		for data in frame_data['point_list']:
+			point = [data['x'], data['y'], data['z'], data['doppler'], data['snr']]
+			points.append(point)
+		return points
 
-    def frame_data_to_cluster_points(self, frame_data):
-        """
-        读取单帧数据，转换为cluster数据结构
-        :param frame_data: 每帧的数据
-        :return: 每一帧转换后的数据
-        """
-        points = []
-        for data in frame_data['point_list']:
-            point = [data['x'], data['y'], data['z'], data['doppler'], data['snr']]
-            points.append(point)
-        return points
+	def cluster_filter_by_noise(self, tem_dict):
+		#去掉噪声
+		try:
+			tem_dict.pop(-1)
+		except:
+			pass
+	
+	def cluster_filter_by_count(self, tem_dict, count):
+		#去除点数较少的类
+		del_list = []
+		for i in tem_dict:
+			if len(tem_dict[i]) < count:
+				del_list.append(i)
+		for key in del_list:
+			tem_dict.pop(key)
+	
+	def compute_cluster_height(self, points):
+		h = Height(1.5)
+		height = h.compute_height(points)
+		return height
+	
+	def compute_cluster_length_width(self, points):
+		cluster_max = np.max(points, axis=0)
+		cluster_min = np.min(points, axis=0)
+		return cluster_max[0]-cluster_min[0],cluster_max[1]-cluster_min[1]
 
-    def cluster_filter_by_noise(self, temp_dict):
-        """
-        过滤噪声
-        :param temp_dict: 聚类集合
-        :return: None
-        """
-        # 去掉噪声
-        try:
-            temp_dict.pop(-1)
-        except:
-            pass
+	def compute_cluster_sum_snr(self, points):
+		sum_snr = 0
+		for point in points:
+			sum_snr += point[4]
+		return sum_snr
 
-    def cluster_filter_by_count(self, temp_dict, count):
-        """
-        去除点数较少的类
-        :param temp_dict: 聚类集合
-        :param count: 聚类所需最少点云数
-        :return: None
-        """
-        del_list = []
-        for i in temp_dict:
-            if len(temp_dict[i]) < count:
-                del_list.append(i)
-        for key in del_list:
-            temp_dict.pop(key)
+	def get_remote_center_point(self, points, origin_center):
+		center_point = []
+		range_points = []
+		for point in points:
+			if 1.4 < point[2] < 1.6:
+				range_points.append(point)
+		if len(range_points) > 0:
+			tem_center = np.mean(range_points, axis=0)
+			center_point = [tem_center[0], tem_center[1], origin_center[2]]
+		else:
+			center_point = [origin_center[0], origin_center[1], origin_center[2]]
+		return center_point
 
-    def compute_cluster_height(self, points):
-        """
-        计算每个聚类的升高
-        :param points: 点云
-        :return: 各个聚类的身高
-        """
-        k = int(len(points) * 0.1) + 1  # 计算前10%点
-        z_list = [points[i][2] for i in range(k)]
-        return round(np.mean(z_list), 3)
+	def compute_cluster_attr(self, tem_dict):
+		cluster_list = []
+		k = 0
+		for i in tem_dict:
+			cluster_dict = {}
+			center_point = np.mean(tem_dict[i], axis=0)
+			varlist = np.var(tem_dict[i], axis=0)
+			cluster_dict['cluster_id'] = k
+			cluster_dict['points_num'] = len(tem_dict[i])
+			#cluster_dict['center_point'] = [center_point[0], center_point[1], center_point[2]]
+			if center_point[1] < 1:
+				cluster_dict['center_point'] = [center_point[0],center_point[1],center_point[2]]
+			else:
+				cluster_dict['center_point'] = self.get_remote_center_point(tem_dict[i], center_point)
+			print(center_point)
+			print(cluster_dict['center_point'])
+			cluster_dict['var_x'] = varlist[0]
+			cluster_dict['var_y'] = varlist[1]
+			cluster_dict['var_z'] = varlist[2]
+			cluster_dict['height'] = self.compute_cluster_height(tem_dict[i])
+			cluster_dict['points'] = tem_dict[i]
+			cluster_dict['length'], cluster_dict['width'] = self.compute_cluster_length_width(tem_dict[i])
+			cluster_dict['sum_snr'] = self.compute_cluster_sum_snr(tem_dict[i])
+			cluster_list.append(cluster_dict)
+			k += 1
+		return cluster_list
 
-    def compute_cluster_length_width(self, points):
-        """
-        计算每个聚类的长度和宽度
-        :param points: 点云
-        :return: 各个聚类的长度和宽度
-        """
-        cluster_max = np.max(points, axis=0)
-        cluster_min = np.min(points, axis=0)
-        return cluster_max[0] - cluster_min[0], cluster_max[1] - cluster_min[1]
+	def cluster_by_tag(self, points, tag):
+		tem_dict = {}
+		#按照类别将点分类
+		key_list = []
+		for i in tag:
+			if i not in key_list:
+				key_list.append(i)
+		#key_list = sorted(key_list)
+		for i in key_list:
+			tem_dict[i] = []
+		for t, point in zip(tag, points):
+			tem_dict[t].append(point)
+		return tem_dict	
 
-    def compute_cluster_attr(self, temp_dict):
-        """
-        计算各个聚类对应的属性，参见__init__函数
-        :param temp_dict: 聚类集合
-        :return: 计算好各个聚类属性后的聚类
-        """
-        cluster_list = []
-        k = 0
-        for i in temp_dict:
-            cluster_dict = {}
-            center_point = np.mean(temp_dict[i], axis=0)
-            varlist = np.var(temp_dict[i], axis=0)
-            cluster_dict['cluster_id'] = k
-            cluster_dict['points_num'] = len(temp_dict[i])
-            cluster_dict['center_point'] = [center_point[0], center_point[1], center_point[2]]
-            cluster_dict['var_x'] = varlist[0]
-            cluster_dict['var_y'] = varlist[1]
-            cluster_dict['var_z'] = varlist[2]
-            cluster_dict['height'] = self.compute_cluster_height(temp_dict[i])
-            cluster_dict['points'] = temp_dict[i]
-            cluster_dict['length'], cluster_dict['width'] = self.compute_cluster_length_width(temp_dict[i])
-            cluster_list.append(cluster_dict)
-            k += 1
-        return cluster_list
+	def divide_cluster_by_count_dopper(self, tem_dict):
+		'''
+		对点数大于100的类，根据dopper单独用dbscan，保留大于40个点的类
+		'''
+		i = 0
+		n = len(tem_dict)
+		print(n)
+		new_key = n
+		while i < n:
+			if len(tem_dict[i]) > 100:
+				print(len(tem_dict[i]))
+				X = np.array(tem_dict[i])
+				#dbscan至少2维，这里给他主动将snr那一列设为同样的值，不影响聚类结果
+				for x_array in X:
+					x_array[4] = 0
+				X = X[:,3:5]
+				db = skc.DBSCAN(eps=0.15, min_samples=15).fit(X)
+				divided_dict = self.cluster_by_tag(tem_dict[i], db.labels_)
+				self.cluster_filter_by_noise(divided_dict)
+				length_divided = len(divided_dict)
+				self.cluster_filter_by_count(divided_dict, 40)
+				print("go here")
+				for j in divided_dict:
+					tem_dict[j+new_key] = divided_dict[j]
+				new_key += length_divided
+				#删除分割的类
+				tem_dict.pop(i)
+			i += 1
+			print("i,n:",i,n)
+	
+	def divide_cluster_by_count_dopper_v2(self, tem_dict):
+		'''
+		对点数大于2倍的类最小点数的类进行分割，按照doppler分割，分割后根据距离过滤
+		'''
+		i = 0
+		n = len(tem_dict)
+		print(n)
+		new_key = n
+		while i < n:
+			center_point = np.mean(tem_dict[i], axis=0)
+			dist = math.sqrt(center_point[0]*center_point[0] + center_point[1]*center_point[1])
+			divide_points_limit = 10
+			if len(tem_dict[i]) > divide_points_limit and (dist < 7 and len(tem_dict[i]) > 2*(1-dist/7)*self.min_cluster_count or dist >= 7):
+				#print(len(tem_dict[i]))
+				X = np.array(tem_dict[i])
+				#dbscan至少2维，这里给他主动将snr那一列设为同样的值，不影响聚类结果
+				for x_array in X:
+					x_array[4] = 0
+				X = X[:,3:5]
+				db = skc.DBSCAN(eps=0.05, min_samples=5).fit(X)
+				divided_dict = self.cluster_by_tag(tem_dict[i], db.labels_)
+				self.cluster_filter_by_noise(divided_dict)
+				length_divided = len(divided_dict)
+				#self.cluster_filter_by_count(divided_dict, 40)
+				#print("go here")
+				for j in divided_dict:
+					tem_dict[j+new_key] = divided_dict[j]
+				new_key += length_divided
+				#删除分割的类
+				tem_dict.pop(i)
+			i += 1
+			#print("i,n:",i,n)
+			
+	def cluster_filter_by_count_and_distance(self, tem_dict):
+		del_list = []
+		for i in tem_dict:
+			center_point = np.mean(tem_dict[i], axis=0)
+			dist = math.sqrt(center_point[0]*center_point[0] + center_point[1]*center_point[1])
+			if dist < 7 and len(tem_dict[i]) < (1-dist/7)*self.min_cluster_count:
+				del_list.append(i)
+		for key in del_list:
+			tem_dict.pop(key)
 
-    def cluster_by_tag(self, points, tag):
-        """
-        根据DBSCAN算法返回的结果标签，将点云和聚类进行对应
-        :param points: 点云
-        :param tag: DBSCAN返回的标签结果
-        :return: 点云和聚类对应好的聚类集合
-        """
-        temp_dict = {}
-        key_list = []
-        for i in tag:
-            if i not in key_list:
-                key_list.append(i)
-        for i in key_list:
-            temp_dict[i] = []
-        for t, point in zip(tag, points):
-            temp_dict[t].append(point)
-        return temp_dict
+	def cluster_filter_by_snr(self, coefficient, tem_dict):
+		pass
 
-    def divide_cluster_by_count_doppler(self, temp_dict):
-        """
-        根据doppler将两个靠的比较近的人进行分割
-        :param temp_dict: 聚类集合
-        :return:None
-        """
-        i = 0
-        n = len(temp_dict)
-        new_key = n
-        while i < n:
-            if len(temp_dict[i]) > 100:
-                print(len(temp_dict[i]))
-                X = np.array(temp_dict[i])
-                # dbscan至少2维，这里给他主动将snr那一列设为同样的值，不影响聚类结果
-                for x_array in X:
-                    x_array[4] = 0
-                X = X[:, 3:5]
-                db = skc.DBSCAN(eps=0.2, min_samples=15).fit(X)
-                divided_dict = self.cluster_by_tag(temp_dict[i], db.labels_)
-                self.cluster_filter_by_noise(divided_dict)
-                length_divided = len(divided_dict)
-                self.cluster_filter_by_count(divided_dict, 40)
-                for j in divided_dict:
-                    temp_dict[j + new_key] = divided_dict[j]
-                new_key += length_divided
-                # 删除分割的类
-                temp_dict.pop(i)
-            i += 1
+	def points_to_cluster_by_tag(self, points, tag):
+		#将点云转换为人
+		tem_dict = self.cluster_by_tag(points, tag)
+		#print(tem_dict)
+		self.cluster_filter_by_noise(tem_dict)
+		#按照点数和dopper对聚类进行分割
+		#self.divide_cluster_by_count_dopper(tem_dict)
+		#self.divide_cluster_by_count_dopper_v2(tem_dict)
+		#过滤
+		#self.cluster_filter_by_count(tem_dict, self.min_cluster_count)
+		self.cluster_filter_by_count_and_distance(tem_dict)
+		#tem_dict = sorted(tem_dict.items(), key = lambda x : x[0]) #按key排序
+		cluster_list = self.compute_cluster_attr(tem_dict)
+		return cluster_list
+	
+	def get_cluster_center_point_list(self):
+		cluster_center_point_list = []
+		for cluster in self.frame_cluster_dict['cluster']:
+			center_point = [cluster['center_point'][0],cluster['center_point'][1]]
+			cluster_center_point_list.append(center_point)
+		return cluster_center_point_list
 
-    def cluster_filter_by_count_and_distance(self, temp_dict):
-        """
-        根据距离和点云数量，将不达标的聚类清理掉
-        :param temp_dict: 聚类集合
-        :return: None
-        """
-        del_list = []
-        for i in temp_dict:
-            center_point = np.mean(temp_dict[i], axis=0)
-            dist = math.sqrt(center_point[0] * center_point[0] + center_point[1] * center_point[1])
-            if dist < 7 and len(temp_dict[i]) < (1 - dist / 7) * self.min_cluster_count:
-                del_list.append(i)
-        for key in del_list:
-            temp_dict.pop(key)
+	def get_height_list(self):
+		cluster_height_list = []
+		for cluster in self.frame_cluster_dict['cluster']:
+			height = cluster['height']
+			cluster_height_list.append(height)
+		return cluster_height_list
+	
+	def show_cluster_dopper(self):
+		#把聚好类的点云的dopper信息输出在终端，观察同一类中dopper的变化
+		#如果同一类中有两个人dopper不同，就可以聚成2个类,目前正在测试
+		print("帧号：%d"%self.frame_cluster_dict['frame_num'])
+		for cluster in self.frame_cluster_dict['cluster']:
+			dopper_list = []
+			for point in cluster['points']:
+				dopper_list.append(round(point[3],3))
+			print(cluster['cluster_id'],dopper_list)
 
-    def cluser_to_person(self, points, tag):
-        """
-        将聚类结果转换为Person
-        :param points: 点云
-        :param tag: DBSCAN返回的聚类标签
-        :return: 人列表
-        """
-        temp_dict = self.cluster_by_tag(points, tag)
-        self.cluster_filter_by_noise(temp_dict)
-        self.cluster_filter_by_count_and_distance(temp_dict)
-        person_list = self.compute_cluster_attr(temp_dict)
-        return person_list
+	def do_clsuter(self, frame_data):
+		self.frame_cluster_dict['frame_num'] = frame_data['frame_num']
+		points = self.frame_data_to_cluster_points(frame_data)
+		tag = self.dbscan_official(points)
+		self.frame_cluster_dict['cluster'] = self.points_to_cluster_by_tag(points, tag)
+		#print(self.frame_cluster_dict)
 
-    def get_cluster_center_point_list(self):
-        """
-        获取每个聚类的中心
-        :return: 聚类中心列表
-        """
-        cluster_center_point_list = []
-        for cluster in self.frame_cluster_dict['cluster']:
-            center_point = [cluster['center_point'][0], cluster['center_point'][1]]
-            cluster_center_point_list.append(center_point)
-        return cluster_center_point_list
+	#test
+	def show_snr(self):
+		print("帧号：%d" % self.frame_cluster_dict['frame_num'])
+		for cluster in self.frame_cluster_dict['cluster']:
+			data = np.array(cluster['points'])
+			print("points_num:", cluster['points_num'])
+			print("cluster_id:", cluster['cluster_id'])
+			print("center_point:", cluster['center_point'])
+			print("all_snr:", cluster['sum_snr'])
+			#print("all_snr", data[:, 4])
 
-    def get_height_list(self):
-        """
-        获取人的身高
-        :return: 每一帧人的身高列表
-        """
-        cluster_height_list = []
-        for cluster in self.frame_cluster_dict['cluster']:
-            height = cluster['height']
-            cluster_height_list.append(height)
-        return cluster_height_list
-
-    def do_clsuter(self, frame_data):
-        """
-        完成聚类和人体识别
-        :param frame_data: 每一帧的数据
-        :return: 聚好类的人
-        """
-        self.frame_cluster_dict['frame_num'] = frame_data['frame_num']
-        points = self.frame_data_to_cluster_points(frame_data)
-        tag = self.dbscan_official(points)
-        self.frame_cluster_dict['cluster'] = self.cluser_to_person(points, tag)
+	def get_sum_snr_list(self):
+		sum_snr_list = []
+		for cluster in self.frame_cluster_dict['cluster']:
+			sum_snr = cluster['sum_snr']/cluster['points_num']
+			sum_snr_list.append(sum_snr)
+		return sum_snr_list
--- 杨家辉-点云聚类/points_filter.py
+++ 杨家辉-点云聚类/points_filter.py
@@ -1,15 +1,11 @@
 class Points_Filter():
-	def __init__(self, z_min, z_max, del_doppler):
+	def __init__(self, z_min, z_max, del_doppler, snr_limit):
 		self.z_min = z_min
 		self.z_max = z_max
 		self.del_doppler = del_doppler
+		self.snr_limit = snr_limit
 	
-	def filter_by_doppler(self, frame_data):
-		"""
-		根据doppler过滤
-		:param frame_data: 每一帧的点云数据
-		:return: None
-		"""
+	def filter_by_dopper(self, frame_data):
 		point_list = []
 		for point_dict in frame_data['point_list']:
 			if point_dict['doppler'] != self.del_doppler:
@@ -18,11 +14,6 @@ class Points_Filter():
 		frame_data['point_num'] = len(point_list)
 		
 	def filter_by_z(self, frame_data):
-		"""
-		根据z进行过滤
-		:param frame_data: 每一帧的点云数据
-		:return: None
-		"""
 		point_list = []
 		for point_dict in frame_data['point_list']:
 			if  self.z_max > point_dict['z'] > self.z_min:
@@ -30,14 +21,17 @@ class Points_Filter():
 		frame_data['point_list'] = point_list
 		frame_data['point_num'] = len(point_list)
 		
+	def snr_filter(self, frame_data):
+		point_list = []
+		for point_dict in frame_data['point_list']:
+			if point_dict['snr'] > self.snr_limit:
+				point_list.append(point_dict)
+		frame_data['point_list'] = point_list
+		frame_data['point_num'] = len(point_list)
 
 	def run_filter(self, frame_data):
-		"""
-		执行过滤
-		:param frame_data: 每一帧的点云数据
-		:return: 这一帧点数
-		"""
-		self.filter_by_doppler(frame_data)
+		self.filter_by_dopper(frame_data)
 		self.filter_by_z(frame_data)
+		self.snr_filter(frame_data)
 		return frame_data['point_num']
 
