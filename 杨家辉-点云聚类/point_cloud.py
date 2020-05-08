8f900e182770e05eb66a48e0cbea0ca170a4e63c
WindStormV <896853536@qq.com>
2020-05-08 20:29:15 +0800

    更新最新聚类可视化

--- 杨家辉-点云聚类/point_cloud.py
+++ 杨家辉-点云聚类/point_cloud.py
@@ -1,8 +1,8 @@
 import sys
-import common
-sys.path.append(r"../龚伟-点云检测")
-# sys.path.append(r"../") #for me
-# import commo #for me
+# import common
+# sys.path.append(r"../龚伟-点云检测")
+sys.path.append(r"../") #for me
+import common #for me
 
 class PointCloud():
     def __init__(self):
@@ -24,26 +24,27 @@ class PointCloud():
                 res.append(point)
         return res
 
+#for qt_show
     @staticmethod
     def get_frame_pointcloud(pointcloud, dofilter):
         if common.queue_for_cluster_transfer.empty():
             return False
         frame_data = common.queue_for_cluster_transfer.get()
-        pointcloud[:] = PointCloud.framedata_to_pointcloud(frame_data)
+        pointcloud = PointCloud.framedata_to_pointcloud(frame_data)
         if dofilter:
-            pointcloud[:] = PointCloud.doppler_filter(pointcloud)
+            pointcloud = PointCloud.doppler_filter(pointcloud)
         return True
 
 
-### for me test
-    '''
+### for cluster_show
+
     @staticmethod
-    def get_frame_pointcloud(pointcloud, dofilter):
-        if commo.point_cloud_show_queue.empty():
+    def get_frame_pointcloud2(pointcloud, dofilter):
+        if common.point_cloud_show_queue.empty():
             return False
-        frame_data = commo.point_cloud_show_queue.get()
+        frame_data = common.point_cloud_show_queue.get()
         pointcloud[:] = PointCloud.framedata_to_pointcloud(frame_data)
         if dofilter:
             pointcloud[:] = PointCloud.doppler_filter(pointcloud)
+        #print(pointcloud[0])
         return True
-'''
