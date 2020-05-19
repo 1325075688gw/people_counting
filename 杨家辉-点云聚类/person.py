e065ab95cd3088b926f4e579f96cb1646f6dbc02
WindStormV <896853536@qq.com>
2020-05-19 11:32:06 +0800

    加入person类

---
+++ 杨家辉-点云聚类/person.py
@@ -0,0 +1,38 @@
+from height import Height
+
+
+class Person:
+    person_length = 0.4
+    person_width = 0.15
+
+    def __init__(self, unidentified_cluster):
+        self.points = unidentified_cluster.points
+        self.center_point = unidentified_cluster.center_point
+        self.cur_height = self.compute_cluster_height()
+        self.doppler_v = unidentified_cluster.doppler_v
+        self.width = unidentified_cluster.width
+        self.length = unidentified_cluster.length
+        self.sum_snr = unidentified_cluster.sum_snr
+        self.avg_snr = unidentified_cluster.avg_snr
+
+    def compute_cluster_height(self):
+        h = Height(1.5)
+        cur_height = h.compute_height(self.points)
+        return cur_height
+
+    @staticmethod
+    def check_person(unidentified_cluster, min_cluster_count, cluster_snr_limit):
+        # 根据输入的点云聚类，判断点云: 不是人  返回0  一个人  返回1  多人 返回2
+
+        # 点云snr和太小，或者点数太少，不是人
+        if unidentified_cluster.sum_snr < cluster_snr_limit * unidentified_cluster.mix_frame_num:
+            return 0
+        if unidentified_cluster.points_num < min_cluster_count * unidentified_cluster.mix_frame_num:
+            return 0
+
+        if unidentified_cluster.length < 0.8 and unidentified_cluster.width < 0.5:
+            return 1
+        if unidentified_cluster.length < 0.6 and unidentified_cluster.width < 0.6:
+            return 1
+
+        return 2
