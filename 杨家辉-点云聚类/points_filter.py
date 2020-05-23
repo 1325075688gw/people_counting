3bafc1d55770d64f2a2cfce38ff0a232892ccd8d
WindStormV <896853536@qq.com>
2020-05-23 14:12:01 +0800

    加入范围滤波

--- 杨家辉-点云聚类/points_filter.py
+++ 杨家辉-点云聚类/points_filter.py
@@ -1,9 +1,13 @@
 class Points_Filter():
-	def __init__(self, z_min, z_max, del_doppler, snr_limit):
+	def __init__(self, z_min, z_max, del_doppler, snr_limit, range_x_min, range_x_max, range_y_min, range_y_max):
 		self.z_min = z_min
 		self.z_max = z_max
 		self.del_doppler = del_doppler
 		self.snr_limit = snr_limit
+		self.range_x_min = range_x_min
+		self.range_x_max = range_x_max
+		self.range_y_min = range_y_min
+		self.range_y_max = range_y_max
 	
 	def filter_by_dopper(self, frame_data):
 		point_list = []
@@ -29,9 +33,18 @@ class Points_Filter():
 		frame_data['point_list'] = point_list
 		frame_data['point_num'] = len(point_list)
 
+	def filter_by_range(self, frame_data):
+		point_list = []
+		for point_dict in frame_data['point_list']:
+			if self.range_x_min < point_dict['x'] < self.range_x_max and self.range_y_min < point_dict['y'] < self.range_y_max:
+				point_list.append(point_dict)
+		frame_data['point_list'] = point_list
+		frame_data['point_num'] = len(point_list)
+
 	def run_filter(self, frame_data):
 		self.filter_by_dopper(frame_data)
 		self.filter_by_z(frame_data)
 		self.snr_filter(frame_data)
+		self.filter_by_range(frame_data)
 		return frame_data['point_num']
 
