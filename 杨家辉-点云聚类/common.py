64076aaf1e9511f28ec8e4ae1ec6864de151dd35
WindStormV <896853536@qq.com>
2020-06-23 18:58:25 +0800

    加入cluster_show

---
+++ 杨家辉-点云聚类/common.py
@@ -0,0 +1,8 @@
+from queue import Queue
+
+# from multiprocessing import Queue
+
+frame_data_queue = Queue() 
+cluster_show_queue = Queue()
+point_cloud_show_queue = Queue()
+queue_for_cluster_transfer = Queue()
